da8956ad7f319df84ce6078120bf397884ca1eb5
Yoann Thébault <thebaultyoann@gmail.com>
2024-01-17 13:41:44 +0100

    manage multiple scan update

--- cyt_evolved/database/first_fill_database.py
+++ cyt_evolved/database/first_fill_database.py
@@ -1,30 +1,47 @@
 import glob
 import os
 import sqlite3
+import datetime
+import subprocess
 
-# Chemin vers les fichiers de la base de données Kismet
+
+#Path to kismet db file (taking the last one)
 db_path = '*.kismet'
 list_of_files = glob.glob(db_path)
 latest_file = max(list_of_files, key=os.path.getctime)
 con_kismet = sqlite3.connect(latest_file)
 cursor_kismet = con_kismet.cursor() 
 
-# Connexion à la base de données 'cyt'
+#Change of DB file
+old_file_name = "cyt.db"
+new_file_name = f"cyt_{datetime.now().strftime('%Y-%m-%d-%H:%M')}.db"
+os.rename(old_file_name, new_file_name)
+
+subprocess.run(["create_database.py"])
+
 con_cyt = sqlite3.connect('cyt.db') 
 cursor_cyt = con_cyt.cursor()
 
-# Requête pour obtenir les données nécessaires de la table 'devices'
+#Store the actual time into a file
+timestamp = int((datetime.now() - datetime.datetime(1970, 1, 1)).total_seconds())
+time_storage_file = "timestamp_storage"
+
+with open(time_storage_file, "w") as file:
+    file.write(str(timestamp))
+
+#Request to get the necessary data
 cursor_kismet.execute('SELECT last_time, devkey, devmac, type FROM devices')
 
 # Itération sur chaque ligne de résultat
 for row in cursor_kismet.fetchall():
     last_time, devkey, devmac, dev_type = row
 
-    # Insertion des données dans la table 'time_presence' de 'cyt'
-    cursor_cyt.execute('INSERT INTO time_presence (first_time_since_cyt_launched, mac_address) VALUES (?, ?)', (last_time, devmac))
+    if last_time > timestamp:
+        # Insertion des données dans la table 'time_presence' de 'cyt'
+        cursor_cyt.execute('INSERT INTO time_presence (first_time_since_cyt_launched, mac_address) VALUES (?, ?)', (last_time, devmac))
 
-    # Insertion des données dans la table 'devices' de 'cyt'
-    cursor_cyt.execute('INSERT INTO devices (device_key, type_of_connection, mac_address) VALUES (?, ?, ?)', (devkey, dev_type, devmac))
+        # Insertion des données dans la table 'devices' de 'cyt'
+        cursor_cyt.execute('INSERT INTO devices (device_key, type_of_connection, mac_address) VALUES (?, ?, ?)', (devkey, dev_type, devmac))
 
 # Validation des modifications
 con_cyt.commit()
--- cyt_evolved/database/second_fill_database.py
+++ cyt_evolved/database/second_fill_database.py
@@ -16,20 +16,30 @@ cursor_cyt = con_cyt.cursor()
 # Requête pour obtenir les données nécessaires de la table 'devices'
 cursor_kismet.execute('SELECT last_time, devkey, devmac, type FROM devices')
 
+#We get back timestamp
+timestamp_file_storage = "timestamp_storage"
+
+with open(timestamp_file_storage, "r") as file:
+    content = file.read()
+
+timestamp = int(content)
+
+
 # Itération sur chaque ligne de résultat
 for row in cursor_kismet.fetchall():
     last_time, devkey, devmac, dev_type = row
-
-    # Vérifier si devmac est déjà présent dans la table 'time_presence' de 'cyt'
-    cursor_cyt.execute('SELECT COUNT(*) FROM time_presence WHERE mac_address = ?', (devmac,))
-    if cursor_cyt.fetchone()[0] > 0:
-        # Mise à jour de 'last_time_since_cyt_launched' si devmac est trouvé
-        cursor_cyt.execute('UPDATE time_presence SET last_time_since_cyt_launched = ? WHERE mac_address = ?', (last_time, devmac))
-    else:
-        # Insertion des données si devmac n'est pas trouvé
-        cursor_cyt.execute('INSERT INTO time_presence (first_time_since_cyt_launched, mac_address) VALUES (?, ?)', (last_time, devmac))
-        cursor_cyt.execute('INSERT INTO devices (device_key, type_of_connection, mac_address) VALUES (?, ?, ?)', (devkey, dev_type, devmac))
-        
+    
+    if timestamp < last_time:
+        # Vérifier si devmac est déjà présent dans la table 'time_presence' de 'cyt'
+        cursor_cyt.execute('SELECT COUNT(*) FROM time_presence WHERE mac_address = ?', (devmac,))
+        if cursor_cyt.fetchone()[0] > 0:
+            # Mise à jour de 'last_time_since_cyt_launched' si devmac est trouvé
+            cursor_cyt.execute('UPDATE time_presence SET last_time_since_cyt_launched = ? WHERE mac_address = ?', (last_time, devmac))
+        else:
+            # Insertion des données si devmac n'est pas trouvé
+            cursor_cyt.execute('INSERT INTO time_presence (first_time_since_cyt_launched, mac_address) VALUES (?, ?)', (last_time, devmac))
+            cursor_cyt.execute('INSERT INTO devices (device_key, type_of_connection, mac_address) VALUES (?, ?, ?)', (devkey, dev_type, devmac))
+            
 # Validation des modifications
 con_cyt.commit()
 
